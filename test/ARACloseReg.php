0348fad9353f8ab543377b78056c299d4cf9a066
Tridiktya Putra <tridiktya@gmail.com>
2022-04-11 10:21:27 +0700

    closeregisARA

---
+++ test/ARACloseReg.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class ARACloseReg extends BaseController
+{
+    public function close_CTF()
+    {
+       return view('closeReg_ara');
+    }
+}
+?>
